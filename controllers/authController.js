dc2db76279d8db41669f697028eef655290c04a9
irudraksh <rudrasoni1414@gmail.com>
2025-06-24 16:32:25 +0530

    payment gateway included

--- controllers/authController.js
+++ controllers/authController.js
@@ -129,10 +129,11 @@ exports.verifyOTP = async (req, res) => {
         await PendingUser.deleteOne({ email });
 
         const token = jwt.sign(
-            { id: newUser._id, role: newUser.role },
-            process.env.JWT_SECRET,
-            { expiresIn: "7d" }
-        );
+  { id: user._id, role: user.role, email: user.email },
+  process.env.JWT_SECRET,
+  { expiresIn: "1h" }
+);
+
 
         res.status(200).json({
             message: "Account verified successfully",
@@ -156,11 +157,13 @@ exports.login = async (req, res) => {
     const isMatch = await bcrypt.compare(password, user.password);
     if (!isMatch) return res.status(400).json({ message: "Invalid credentials" });
 
+ 
     const token = jwt.sign(
-      { id: user._id, role: user.role },
-      process.env.JWT_SECRET,
-      { expiresIn: "1h" }
-    );
+  { id: user._id, role: user.role, email: user.email },
+  process.env.JWT_SECRET,
+  { expiresIn: "1h" }
+);
+
 
     res.cookie("token", token, {
       httpOnly: true,
