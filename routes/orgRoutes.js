0b1716d8eb6ec1ba2cc1a6b0c1f4dfce4ffd9a42
irudraksh <rudrasoni1414@gmail.com>
2025-07-02 19:57:24 +0530

    some changes done

--- routes/orgRoutes.js
+++ routes/orgRoutes.js
@@ -5,21 +5,21 @@ const isVerifiedOrg = require('../middleware/isVerifiedOrg');
 const { authenticate } = require('../middleware/authMiddleware');
 
 // Protect with auth + verified org
-router.post('/org/jobs', authenticate,  jobController.createJob);
-router.put('/org/jobs/:jobId', authenticate,  jobController.updateJob);
-router.delete('/org/jobs/:jobId', authenticate,  jobController.deleteJob);
+router.post('/org/jobs', authenticate, isVerifiedOrg, jobController.createJob);
+router.put('/org/jobs/:jobId', authenticate,isVerifiedOrg,  jobController.updateJob);
+router.delete('/org/jobs/:jobId', authenticate,isVerifiedOrg,  jobController.deleteJob);
 // routes/application.js
-router.get('/org', authenticate,  jobController.getApplicationsForOrg);
-router.patch('/applications/:id/status', authenticate,  jobController.updateStatus);
+router.get('/org', authenticate, isVerifiedOrg, jobController.getApplicationsForOrg);
+router.patch('/applications/:id/status', authenticate,  isVerifiedOrg,jobController.updateStatus);
 
-router.get("/recruiter", authenticate, jobController.getRecruiter);
-router.put("/update-recruiter", authenticate, jobController.updateRecruiter);
+router.get("/recruiter", authenticate,isVerifiedOrg, jobController.getRecruiter);
+router.put("/update-recruiter", authenticate, isVerifiedOrg,jobController.updateRecruiter);
 // Get all applications for a job, optionally filtered by status
-router.get('/applications/job/:jobId', authenticate,  jobController.getApplicationsByJobAndStatus);
+router.get('/applications/job/:jobId', authenticate, isVerifiedOrg, jobController.getApplicationsByJobAndStatus);
 
 // router.put('/applications/:applicationId/status', authenticate,  jobController.updateApplicationStatus);
-router.get('/org/all', authenticate,  jobController.getAllJobsByOrganization);
-router.get('/org/drafts', authenticate,  jobController.getDraftJobsByOrganization);
+router.get('/org/all', authenticate,isVerifiedOrg,  jobController.getAllJobsByOrganization);
+router.get('/org/drafts', authenticate, isVerifiedOrg, jobController.getDraftJobsByOrganization);
 router.get('/jobs/:id/preview', jobController.getJobByIdForPreview);
 
 router.get('/jobs/active/:id', jobController.getActiveJobById); // public route
